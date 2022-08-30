a1b45ff94b9db389d9f44cc6d874b030c63cfd65
rizkyananda <rizky.ananda@detik.com>
2022-08-30 21:09:56 +0700

    move ui theme directory

--- app/src/main/java/com/ekyrizky/stonkscalculator/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/ekyrizky/stonkscalculator/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.ekyrizky.stonkscalculator.ui.theme
+package com.ekyrizky.stonkscalculator.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
